3f8d547667fc13b0f66fd1dc68c683dbdbc20437
Samer Saida <ssaida@gmail.com>
2024-12-23 09:48:55 +0200

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,22 @@
+
+public class Number {
+
+	public Number(int n) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Number() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int howManyHits(Number user) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int howManyAlmost(Number user) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ Tester.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Tester {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		Number comp = new Number();
+		boolean win = false;
+		
+		do {
+			System.out.println("Enter your guess:");
+			int n = in.nextInt();
+			Number user = new Number(n);
+			int hits = comp.howManyHits(user);
+			int almost = comp.howManyAlmost(user);
+			if(hits == 4)
+				win = true;
+			else win = false;
+			System.out.println("Hits "+hits+" Almost: "+almost);
+		}while(!win);
+		
+		in.close();
+	}
+}
